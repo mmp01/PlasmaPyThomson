329a69b2caa9877e10ed13b94953afeac3156ad4
mmp01 <mmpokornik@gmail.com>
2023-03-19 23:09:21 -0700

    Update thomson.py
    changed the infract norm to 1 error to normalizing. Ran into lots of floating point errors.

--- plasmapy/diagnostics/thomson.py
+++ plasmapy/diagnostics/thomson.py
@@ -430,7 +430,7 @@ def spectral_density(
     else:
         ifract = np.asarray(ifract, dtype=np.float64)
         if np.sum(ifract) != 1:
-            raise ValueError(f"The provided ifract does not sum to 1: {ifract}")
+            ifract=ifract/np.sum(ifract)
 
     if probe_vec is None:
         probe_vec = np.array([1, 0, 0])
